b889a5fcf021688f4fe6c9fe816232fac9307d91
selmi zaid <selmizaid@gmail.com>
2018-06-12 23:26:08 +0100

    first push 4 azure

---
+++ server.ts
@@ -0,0 +1,46 @@
+import 'zone.js/dist/zone-node';
+import 'reflect-metadata';
+import { renderModuleFactory } from '@angular/platform-server';
+const express = require('express');
+import { readFileSync } from 'fs';
+import { enableProdMode } from '@angular/core';
+const { AppServerModuleNgFactory } = require('./dist/user-web-v2-server/main');
+enableProdMode();
+
+const app = express();
+
+
+var localStorage = require('localStorage')
+
+
+
+localStorage.setItem('myFirstKey', 'myFirstValue');
+console.log(localStorage.getItem('myFirstKey'));
+
+const indexHtml = readFileSync(__dirname + '/dist/browser/index.html', 'utf-8').toString();
+
+
+app.get('*.*', express.static(__dirname + '/dist/browser', {
+    maxAge: '1y'
+}));
+
+
+app.route('*').get((req, res) => {
+
+    renderModuleFactory(AppServerModuleNgFactory, {
+        document: indexHtml,
+        url: req.url
+    })
+        .then(html => {
+            res.status(200).send(html);
+        })
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(500);
+        });
+
+});
+
+app.listen(9000, () => {
+    console.log(`Angular Universal Node Express server listening on http://localhost:9000`);
+});
